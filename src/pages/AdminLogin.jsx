259dadbe6e94af63d24955599961975586d6f75d
Daniela <daniela.primacov@yahoo.com>
2025-09-19 10:33:57 +0200

    Changing style of the admin login page

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -23,8 +23,10 @@ export default function AdminLogin() {
   return (
     <div className="bg-blue-200 min-h-screen pt-30">
       <div className="flex flex-col">
-        <div className="w-full max-w-sm mx-auto p-6 border rounded">
-          <h1 className="text-xl text-center font-semibold mb-4">Admin</h1>
+        <div className="w-full max-w-sm mx-auto p-6 border border-blue-300 rounded bg-blue-300 shadow-md">
+          <h1 className="text-xl text-center text-blue-600 tracking-wider font-semibold mb-4">
+            Admin
+          </h1>
           {err && <p className="text-red-600 mb-2">{err}</p>}
           <form onSubmit={onSubmit}>
             <input
@@ -32,7 +34,7 @@ export default function AdminLogin() {
               value={pwd}
               onChange={(e) => setPwd(e.target.value)}
               placeholder="Passwort"
-              className="w-full border p-2 rounded mb-3"
+              className="w-full border border-white/40 p-2 rounded mb-3 focus:outline-none focus:border-blue-500"
             />
             <button className="w-full bg-blue-600 text-white p-2 rounded cursor-pointer hover:bg-blue-500 transition duration-300">
               Anmelden
@@ -42,7 +44,7 @@ export default function AdminLogin() {
 
         <Link
           to="/"
-          className="text-center mt-5 hover:text-blue-500 transition duration-300"
+          className="text-center text-blue-800 mt-5 hover:text-blue-500 transition duration-300"
         >
           Zurück
         </Link>
